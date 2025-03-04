7693ff51e82b07897536722c56ddef6db34dbfac
Humberto Rodriguez <humbertorodriguez@Humbertos-MacBook-Pro.local>
2025-03-03 18:24:08 -0600

    prevent scrolling when hamburger menu activated

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ window.addEventListener('resize', function(){
         document.getElementById('toggle').checked = false;
     }
 })
+
+document.addEventListener('DOMContentLoaded', () => {
+    const toggle = document.getElementById('toggle');
+    
+    toggle.addEventListener('change', () => {
+        if (toggle.checked) {
+            document.body.style.overflow = 'hidden';
+        } else {
+            document.body.style.overflow = 'auto';
+        }
+    });
+});
